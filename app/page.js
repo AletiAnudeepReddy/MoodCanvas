dadaca1d226ffe35317ac073934cdeffdf280a85
anudeep096 <aletianudeepreddy8@gmail.com>
2025-07-07 18:40:06 +0530

    fd

--- app/page.js
+++ app/page.js
@@ -3,7 +3,16 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]">
+      <div className="container mx-auto grid grid-cols-2 mt-32">
+        <div className="col1">
+
+        </div>
+        <div className="col2 flex items-center justify-center mt-10">
+          <Image width={300} height={500} src={"/wallpage.jpeg"} alt="m" className="rounded-xl"/>
+        </div>
+      </div>
+    </div>
      </>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -12,19 +12,19 @@ const Navbar = () => {
     return (
         <div className='container mx-auto flex flex-row justify-between items-center h-30 p-10 px-20'>
             <div className='logo flex flex-row items-center gap-3'>
-                <MdWallpaper size={42} color="#ec4899"/>
+                <MdWallpaper size={46}  className='text-purple-600 fill-purple-600'/>
                 <Link href={"/"}><h1 className="text-4xl font-serif tracking-wide font-bold bg-gradient-to-r from-indigo-500 via-pink-600 to-purple-500 text-transparent bg-clip-text">
                     MoodCanvas
                 </h1></Link>
             </div>
             <div>
                 <div className="flex p-2 items-center gap-4">
-                    <HomeIcon className="hover:w-9 hover:h-9 w-8 h-8 text-black" />
+                    <HomeIcon className="hover:w-9 hover:h-9 w-8 h-8 text-purple-700" />
                     
                     
-                    <PlayCircleIcon className="hover:w-9 hover:h-9 w-8 h-8 text-black" />
-                    <FaGithub className="hover:w-9 hover:h-9 w-8 h-8 text-gray-800" />
-                    <ArrowRightOnRectangleIcon className="hover:w-9 hover:h-9 w-8 h-8 text-black" />
+                    <PlayCircleIcon className="hover:w-9 hover:h-9 w-8 h-8 text-pink-700" />
+                    <FaGithub className="hover:w-9 hover:h-9 w-8 h-8 text-gray-700" />
+                    <ArrowRightOnRectangleIcon className="hover:w-9 hover:h-9 w-8 h-8 text-green-700" />
                 </div>
             </div>
         </div>
